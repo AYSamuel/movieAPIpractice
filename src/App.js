a0d5728f67c584a648a903e99d216f768e4e0e9c
unknown <aysamuel007@gmail.com>
2023-02-06 02:12:29 +0100

    movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import MovieCard from './component/MovieCard';
+import SearchIcon from './search.svg';
 
-function App() {
+const API_URL = 'http://www.omdbapi.com?apikey=a874f9a4';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  //Axios for fetching data syntax, rather than using fetch. (dont forget to npm install axios and then import it before using this syntax)
+  const searchMovies = async (title) => {
+    return axios
+      .get(`${API_URL}&s=${title}`)
+      .then((response) => {
+        // handle success
+        const data = response.data;
+        setMovies(data.Search);
+      })
+      .catch((error) => {
+        // handle error
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    searchMovies('');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Saminex Movies</h1>
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}></input>
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => searchMovies(searchTerm)}></img>
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>Search for your favorite movie</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
